207e81815f8e91dfbf287eb87a7275e79337d0b3
Lukman Arogundade <princedotexe@gmail.com>
2017-09-05 14:44:24 -0500

    Update Prog6.java

--- Lesson - 2- Home Work Assignment/Prog6.java
+++ Lesson - 2- Home Work Assignment/Prog6.java
@@ -1,16 +1,34 @@
 package fpp;
 
+import java.util.Arrays;
+
 public class Prog6 {
 
 	public static void main(String[] args) {
 		String[] arrayWithDuplicate = {"horse", "dog", "cat", "horse","dog"};
 		String[] arrayWithoutDuplicate = new String[5] ;
 		
+		int k=0;
 		for(String item :arrayWithDuplicate)
 		{
-		//	if (item.equals(arrayWithDuplicate))
+		  if (!checkForDuplicate(item,arrayWithoutDuplicate))
+		  {
+			  arrayWithoutDuplicate[k]= item;
+			  ++k;
+		  }
 		}
-
+		System.out.println(Arrays.toString(arrayWithoutDuplicate));
+	}
+	
+	public static boolean checkForDuplicate(String item, String[] arr)
+	{
+		for(String element : arr)
+		{
+			if (item.equals(element))
+				return true;
+		}
+		
+		return false;
 	}
 
 }
