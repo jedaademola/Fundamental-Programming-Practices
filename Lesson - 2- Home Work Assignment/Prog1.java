64ae2e039bc7501b9cec920c6950745509854276
Lukman Arogundade <princedotexe@gmail.com>
2017-09-06 08:32:41 -0500

    Update Prog1.java

--- Lesson - 2- Home Work Assignment/Prog1.java
+++ Lesson - 2- Home Work Assignment/Prog1.java
@@ -7,6 +7,7 @@ public class Prog1 {
 		int x=RandomNumbers.getRandomInt(1, 9);
 		System.out.println("x:"  + x);
 		System.out.println("πx:"  + Math.pow(Math.PI, x));
+		
 		//2. get a random number y in the range 3 .. 14 and compute yπ.
 		int y= RandomNumbers.getRandomInt(3, 14);
 		System.out.println("y:"  + y);
@@ -20,5 +21,7 @@ x:7
 πx:3020.2932277767914
 y:14
 yπ:3987.194457670312
-
 */
+
+
+
