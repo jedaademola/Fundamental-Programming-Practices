27228150a0b19bcf61e594ba7bcfd928529efad1
Lukman Arogundade <princedotexe@gmail.com>
2017-09-06 14:11:39 -0500

    Update Prog5.java

--- Lesson - 2- Home Work Assignment/Prog5.java
+++ Lesson - 2- Home Work Assignment/Prog5.java
@@ -12,53 +12,52 @@ public class Prog5 {
 			}
 		}
 
-		/*
-		 * 23 +33 36 +90 78 +61 23 +91 2 +40 91 +75 57 +53 73 +20
-		 */
-/*
-		elementarySchoolData[0][0] = 23;
-		elementarySchoolData[0][1] = 33;
-		elementarySchoolData[0][2] = 36;
-		elementarySchoolData[0][3] = 90;
-
-		elementarySchoolData[1][0] = 78;
-		elementarySchoolData[1][1] = 61;
-		elementarySchoolData[1][2] = 23;
-		elementarySchoolData[1][3] = 91;
-
-		elementarySchoolData[2][0] = 2;
-		elementarySchoolData[2][1] = 40;
-		elementarySchoolData[2][2] = 91;
-		elementarySchoolData[2][3] = 75;
-
-		elementarySchoolData[3][0] = 57;
-		elementarySchoolData[3][1] = 53;
-		elementarySchoolData[3][2] = 73;
-		elementarySchoolData[3][3] = 20;
-*/
-		for (int a = 0; a < len; ++a) {
+				for (int a = 0; a < len; ++a) {
+			
 			for (int b = 0; b < len; ++b) {
 
-				if (a % 2 == 1 && a > 0) {
+				if (a % 2 == 1 ) {
 
 					 System.out.printf("+ %d \t ", elementarySchoolData[a][b] );
-					
-					//System.out.print("+" + elementarySchoolData[a][b] + "\t");
+					 
+					 if(b == len -1)
+					 {
+						 int k= 0;
+						 System.out.println();
+						 while( k < len)
+						 {
+						 //System.out.print("____\t  ____\t ____\t ____");
+							 System.out.print("____\t ");
+							 ++k;
+						 }
+						 System.out.println();
+						
+					 }
+
 
 				} else
 
-					System.out.printf(" %d \t", elementarySchoolData[a][b]);
+					System.out.printf("  %d \t ", elementarySchoolData[a][b]);
 				
-				//  System.out.print("____");
-
+				
 			}
 
 			System.out.println();
-			// if (a%2 == 1 && a > 0) System.out.println( "___" + "\t");
-			// System.out.println();
+			
 
 		}
 
 	}
 
 }
+/*
+   34 	   70 	   73 	   72 	 
++ 54 	 + 39 	 + 69 	 + 71 	 
+____	 ____	 ____	 ____	 
+
+  31 	   15 	   52 	   39 	 
++ 9 	 + 63 	 + 37 	 + 44 	 
+____	 ____	 ____	 ____	 
+
+
+ */ 
