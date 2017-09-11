b7add8c1dc18c3616f3bb2d5afe10936cf132500
Lukman Arogundade <princedotexe@gmail.com>
2017-09-11 13:53:19 -0500

    add more unit test

--- Lesson-4-Lab-Recursion/Lesson3UnitTests.java
+++ Lesson-4-Lab-Recursion/Lesson3UnitTests.java
@@ -23,5 +23,23 @@ public class Lesson3UnitTests {
 		String Eoutput = "ada";
 		assertEquals(Ooutput,Eoutput);
 	}
+	@Test
+	public void testBinarySearch() {
+		
+	   int[] integerArray = {4,5,6,8,9,11,12,17,18,20,21,23,25,27,30,31,33};
+		RecursiveBinarySearch recursion = new RecursiveBinarySearch();
+		int Ooutput = recursion.binarySearch(12, integerArray);
+		int Eoutput = 6;
+		assertEquals(Ooutput,Eoutput);
+	}
 
+	@Test
+	public void testBinarySearch2() {
+		
+	   int[] integerArray = {4,5,6,8,9,11,12,17,18,20,21,23,25,27,30,31,33};
+		RecursiveBinarySearch recursion = new RecursiveBinarySearch();
+		int Ooutput = recursion.binarySearch(10, integerArray);
+		int Eoutput = -1;
+		assertEquals(Ooutput,Eoutput);
+	}
 }
