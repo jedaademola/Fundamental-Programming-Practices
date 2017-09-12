3e579e55a77085771d12f44109da2eb1000df910
Lukman Arogundade <jedaademola@users.noreply.github.com>
2017-09-12 17:08:11 -0500

    rename subfolder

---
+++ Lesson-5-Lab-Inheritance-Interface-Polymorphism/Shape.java
@@ -0,0 +1,23 @@
+package com.fpp;
+
+public class Shape {
+	
+	protected String color;
+	
+	public Shape (String color)
+	{
+		this.color = color;
+	}
+
+	
+
+	public double calculateArea()
+	{
+		return 0.0;
+	}
+	
+	public double calculatePerimter()
+	{
+		return 0.0;
+	}
+}
