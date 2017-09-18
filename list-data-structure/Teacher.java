131e43adac41244c2714797017b3fdbf0700f760
Lukman Arogundade <princedotexe@gmail.com>
2017-09-18 14:15:38 -0500

    Update Teacher.java

--- list-data-structure/Teacher.java
+++ list-data-structure/Teacher.java
@@ -1,15 +1,18 @@
 package com.fpp.lesson8;
 
-public final class Teacher  {
+public final class Teacher  implements EmployeeData{
+	
 	private double bonus;
 	private String name;
 	private double salary;
+	
 	public Teacher(String name, double salary, double bonus) {
 		this.name = name;
 		this.salary = salary;
 		this.bonus = bonus;
 	}
 	
+	//@Override
 	public double getSalary() {
 		return salary + bonus;
 	}
