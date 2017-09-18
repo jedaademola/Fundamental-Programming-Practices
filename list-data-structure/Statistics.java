f6d21e5f28d39274729a83f99c7c41d846a31dd4
Lukman Arogundade <princedotexe@gmail.com>
2017-09-18 14:14:57 -0500

    Update Statistics.java

--- list-data-structure/Statistics.java
+++ list-data-structure/Statistics.java
@@ -6,8 +6,13 @@ public class Statistics {
 	 * Polymorphically computes and returns the sum
 	 * of all the salaries of all the staff/teachers in the list.
 	 */
-	public static double computeSumOfSalaries(List/*<implement>*/ aList) {
-		return 0.0;
+	public static double computeSumOfSalaries(List <EmployeeData> aList) {
+		double totalSalary=0.0;
+		
+		for(EmployeeData e:aList)
+			totalSalary +=e.getSalary();
+		
+		return totalSalary;
 		//implement
 		//compute sum of all salaries of people in aList and return
 	}
