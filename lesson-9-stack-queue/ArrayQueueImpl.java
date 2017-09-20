cd79b7736d660418ea81582dd73b732edf9edb28
Lukman Arogundade <princedotexe@gmail.com>
2017-09-20 14:15:03 -0500

    Implement Queue using Array

---
+++ lesson-9-stack-queue/ArrayQueueImpl.java
@@ -0,0 +1,83 @@
+package com.fpp.lesson8;
+
+public class ArrayQueueImpl {
+
+	private int[] arr = new int[10];
+	private int front = -1;
+	private int rear = 0;
+	
+	private int size = 0;
+
+	public int peek() 
+	{
+		if (isEmpty())
+		{
+			System.out.println("Queue is Empty");
+		    return -1;
+		}
+		else
+		{
+			int topElement = arr[front];
+			return topElement;
+			
+		}
+	}
+
+	public void pint()
+	{
+		if(isEmpty())
+		{
+			System.out.println("Queue is Empty");
+			return;
+		}
+		for(int k=front;k< size;++k)
+		{
+			System.out.print(arr[k]);
+		}
+		
+	}
+	public void enqueue(int obj) {
+		if(size == arr.length) resize();
+		if (size == 0) ++front;
+		
+		++size;
+		arr[rear]= obj;
+		++rear;
+	}
+
+	public int dequeue() {
+		
+		if (isEmpty())
+		{
+			System.out.println("Queue is Empty");
+		    return -1;
+		}
+		else
+		{
+			int topElement = arr[front];
+		    ++front;
+			--size;
+			return topElement;
+			
+		}
+	
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	private void resize() {
+		int len = arr.length;
+		int newlen = 2*len;
+		int[] temp = new int[newlen];
+		System.arraycopy(arr,0,temp,0,len);
+		arr = temp;
+	}
+	
+	
+}
---
+++ lesson-9-stack-queue/ArrayQueueMain.java
@@ -0,0 +1,30 @@
+package com.fpp.lesson8;
+
+public class ArrayQueueMain {
+
+	public static void main(String[] args) {
+		ArrayQueueImpl queue = new ArrayQueueImpl();
+		queue.enqueue(9);
+		queue.enqueue(8);
+		queue.enqueue(7);
+		queue.enqueue(6);
+		queue.enqueue(5);
+
+		queue.pint();
+		System.out.println();
+		System.out.println("Queue size:" + queue.size());
+		
+		System.out.println("Dequeue element:" + queue.dequeue());
+		queue.pint();
+		
+		System.out.println();
+		System.out.println("Queue size:" + queue.size());
+		
+		System.out.println("Peek element:" + queue.peek());
+		queue.pint();
+	
+	
+	
+	}
+
+}
