28084f28be238fa3818d96a185e7533801b57d6c
Lukman Arogundade <jedaademola@users.noreply.github.com>
2017-09-21 16:50:54 -0500

    updates

--- lesson-9-stack-queue/ArrayQueueMain.java
+++ lesson-9-stack-queue/ArrayQueueMain.java
@@ -1,5 +1,7 @@
 package com.fpp.lesson8;
 
+import com.fpp.lesson8and9.ArrayQueueImpl;
+
 public class ArrayQueueMain {
 
 	public static void main(String[] args) {
@@ -10,21 +12,25 @@ public class ArrayQueueMain {
 		queue.enqueue(6);
 		queue.enqueue(5);
 
+		System.out.print("Queue Elements:");
 		queue.pint();
 		System.out.println();
 		System.out.println("Queue size:" + queue.size());
 		
-		System.out.println("Dequeue element1:" + queue.dequeue());
+		System.out.println("Dequeue element:" + queue.dequeue());
+		System.out.print("Queue Elements:");
 		queue.pint();
 		
 		System.out.println();
 		System.out.println("Queue size:" + queue.size());
 		
 		System.out.println("Peek element:" + queue.peek());
+		System.out.print("Queue Elements:");
 		queue.pint();
 		
 		System.out.println();
-		System.out.println("Dequeue element2:" + queue.dequeue());
+		System.out.println("Dequeue element:" + queue.dequeue());
+		System.out.print("Queue Elements:");
 		queue.pint();
 		System.out.println();
 		System.out.println("Queue size:" + queue.size());
@@ -34,3 +40,19 @@ public class ArrayQueueMain {
 	}
 
 }
+
+/*
+OUTPUT
+------------
+Queue Elements:98765
+Queue size:5
+Dequeue element:9
+Queue Elements:8765
+Queue size:4
+Peek element:8
+Queue Elements:8765
+Dequeue element:8
+Queue Elements:765
+Queue size:3
+
+ */
