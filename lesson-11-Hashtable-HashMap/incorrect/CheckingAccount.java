d34f70ff9e5ec35c7f09fb1128a950924a4034c0
Lukman Arogundade <princedotexe@gmail.com>
2017-09-22 14:07:09 -0500

    initial commit

---
+++ lesson-11-Hashtable-HashMap/incorrect/CheckingAccount.java
@@ -0,0 +1,13 @@
+package com.fpp.lesson11.incorrect;
+
+
+
+public final class CheckingAccount {
+	public CheckingAccount(double balance) {
+		this.balance = balance;
+	}
+	double balance;
+	public double getBalance() {
+		return balance;
+	}
+}
